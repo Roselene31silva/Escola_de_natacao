703e873a69797ff558d4ca864ebd156dd6cb323a
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-03-31 19:53:35 -0300

    exercicio01

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 let nomeCompleto = prompt("Informe seu nome completo")
 console.log(nomeCompleto)
 
-let cor = prompt("Informe uma cor: \n1 azul \n 2 vermelho \n 3 preto")
-let corEscolhida = null
+function fnTrocarCorDeFundo(){
+    let cor = prompt("Informe uma cor: \n1 azul \n 2 vermelho \n 3 preto")
+    let corDeFundo = null
 
-if(cor == "1"){
-    corDeFundo = "blue"
-}else if(cor == "2"){
-    corDeFundo = "red"
-}else if(cor == "3"){
-    corDeFundo
-}else{
-    corDeFundo = "yellow"
+    if (cor == "1") {
+        corDeFundo = "blue"
+    } else if (cor == "2") {
+        corDeFundo = "red"
+    } else if (cor == "3") {
+        corDeFundo
+    } else {
+        corDeFundo = "yellow"
+    }
+
+    document.body.style.backgroundColor = corDeFundo
 }
 
-document.body.style.backgroundColor = corDeFundo
+fnTrocarCorDeFundo()
---
+++ exercicio-01/ex01.js
@@ -0,0 +1,4 @@
+let num1 = prompt("informe o primeiro numero")
+let num2 = prompt("Informe o segundo numero")
+let resultado = parseFloat(num1) + parseFloat(num2)
+alert("O rsultado é: " + resultado)
