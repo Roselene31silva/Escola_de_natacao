c6e043cbbcb88bbe0d217f3a6d839db2a30cb2d9
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-03-31 19:14:23 -0300

    refaturacao-de-codigo

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
-console.log("Roselene")
-console.log(42)
-console.warn("atenção")
-console.error("um erro")
-/*
-3 forma de criar variáveis
-var
-let --> vamos usar essa
-const
-*/
-let nome = "Roselene"
-let idade = 42
-let telefone = null
-console.log(nome, idade)
-nome = "Roselene Silva"
-console.log(nome)
-
 let nomeCompleto = prompt("Informe seu nome completo")
 console.log(nomeCompleto)
-//alert("Seja bem vindo + nomeCompleto")
-//document.write("Seja bem vindo(a) " + nomeCompleto)
+
 let cor = prompt("Informe uma cor: \n1 azul \n 2 vermelho \n 3 preto")
 let corEscolhida = null
 
