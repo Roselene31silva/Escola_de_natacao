d8d5916f4b09be62b6335f2b8deafe3d0f9ec287
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-03-31 22:13:36 -0300

    exercicio04

---
+++ exercicio03/ex03.js
@@ -0,0 +1,10 @@
+let nome = prompt("Informe seu nome")
+let idade = prompt("Informe sua idade")
+
+if(idade < 18){
+    alert("Olá " + nome + "você ainda não pode fazer aulas de direção")
+}
+
+else if(idade >=18){
+    alert("Ola " + nome + ", Seja bem vindo(a) a nossa escola! ja fez sua matricula")
+}
---
+++ exercicio04/ex04.js
@@ -0,0 +1,6 @@
+let valor1 = parseFloat(prompt("Digite o primeiro valor:"))
+let valor2 = parseFloat(prompt("Digite o segundo valor:"))
+let valor3 = parseFloat(prompt("Digite o terceiro valor:"))
+
+let media = (valor1 + valor2 + valor3) / 3
+alert("O Resultado da media é: " + media)
