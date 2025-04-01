6024e0876abdebce223d4473ea0ecc2ebb8c322b
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-03-31 21:11:53 -0300

    exercicio02certo

--- exercicio-02/ex02.js
+++ exercicio-02/ex02.js
@@ -1,13 +1,13 @@
-let num1 = (prompt("Digite o primeiro número:")
-let num2 = (prompt("Digite o segundo número:")
-let operador = prompt("Digite o operador matematico: + - / * ")
+let num1 = prompt("Digite o primeiro número: ")
+let num2 = prompt("Digite o segundo número: ")
+let operador = prompt("Informe o operador matematico: + - / * ")
 
 if (operador == "+") {
     let resultado = parseFloat(num1) + parseFloat(num2)
     alert("O resultado da soma é: " + resultado)
 } else if (operador == "-") {
     let resultado = parseFloat(num1) - parseFloat(num2)
-    alert("O resultado da subtraçaõ é" + resultado)
+    alert("O resultado da subtraçaõ é: " + resultado)
 } else if (operador == "/") {
     let resultado = parseFloat(num1) / parseFloat(num2)
     alert("O resultado da divisão é: " + resultado);
